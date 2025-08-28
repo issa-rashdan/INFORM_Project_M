48aa70a938b049bcc6cb015d7f11218293710307
issa-rashdan <73997157+issa-rashdan@users.noreply.github.com>
2025-08-28 13:49:34 +0200

    Add files via upload

---
+++ adaptation_method/model_config.py
@@ -0,0 +1,22 @@
+from Inform_project_new.adaptation_method.Autoencoder import ResNetAutoEncoder, Autoencoder
+import wandb
+def Configuration(model_name = ''):    
+    run = wandb.init(
+        project= 'Adapter',
+        name = f'{model_name}_run',
+        config = {
+            'batch_size': 32,
+            'learning_rate': 1e-3,        
+            'epochs': 30,
+            'Weight_decay': 1e-4
+        }, 
+    )
+    return run
+
+
+
+
+
+
+
+
